6e630f05cd98ff1b4f9a2f7935afccbd1a8b8634
Iuliia Goriun <goriunyuliya@gmail.com>
2023-02-27 21:39:38 +0300

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,11 +1,11 @@
 import axios from 'axios';
 
 axios.defaults.baseURL = 'https://api.themoviedb.org/3';
-//const API_KEY = 'b72e97d50f503cf310444389e0d21ec6';
+//const API_KEY = '58100b962b8f017dc01cc98365b716db';
 
 export const requestTrendingMovies = async () => {
   const { data } = await axios.get(
-    `trending/movie/day?key=b72e97d50f503cf310444389e0d21ec6&page=${1}`
+    `/trending/movie/day?key=58100b962b8f017dc01cc98365b716db&page=${1}`
   );
   return data.results;
 };
